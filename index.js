0ce8850ab49ea91de96f2f955c09b1312bd169a3
Felice Forby <feliceforby@gmail.com>
2019-02-20 23:55:32 +0000

    set up templates and register helpers

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 function init() {
   //put any page initialization/handlebars initialization here
+
+  // Set up recipe form
+  renderRecipeForm();
+
+  // Register partials
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+
+  // Register helpers
+  Handlebars.registerHelper('displayIngredient', function() {
+    return new Handlebars.SafeString('<li name="ingredients">' + this + '</li>');
+  });
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function renderRecipeForm() {
+  let main = document.getElementById('main');
+  let recipeFormTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML);
+  let recipeFormHTML = recipeFormTemplate();
+
+  main.innerHTML += recipeFormHTML;
+}
+
+function handleSubmit() {
+  buildRecipeData();
+}
+
+function buildRecipeData() {
+  let title = document.getElementById("name").value;
+  let description = document.getElementById("description").value;
+  let ingredientNodes = document.getElementsByName("ingredients");
+  let ingredients = [];
+
+  for (let i = 0; i < ingredientNodes.length; i++) {
+    ingredients.push(ingredientNodes[i].value);
+  }
+
+  let recipe = {
+    title: title,
+    description: description,
+    ingredients: ingredients
+  }
+  return recipe;
+}
